aa0bd7bd321314d02179a27fcd59bf629519cf1a
lucas bigeardel <lucas.bigeardel@gmail.com>
2018-02-25 20:11:28 +0100

    Update FirmataCmdQxEventHandler.java

--- iorx/src/main/java/ubiquisense/iorx/protocols/firmata/FirmataCmdQxEventHandler.java
+++ iorx/src/main/java/ubiquisense/iorx/protocols/firmata/FirmataCmdQxEventHandler.java
@@ -1,37 +1,3 @@
-/***********************************************************************************
-FirmataCmdUtils.java * Ubiquisense - A smart ambient utilities framework 
- * 
- * Copyright (c) 2008 - 2015, Lucas Bigeardel
- * 
- * The library is released under:
- * 
- * A) LGPL
- * 
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- * 
- * B) EPL
- * 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *     Lucas Bigeardel <lucas.bigeardel@gmail.com> - Initial API and implementation
- ***********************************************************************************/
 
 package ubiquisense.iorx.protocols.firmata;
 
