7a427132cf707f0fc2f504e6edc4fbcd7a16350b
lucas <lucas@LAPTOP-2SKH5AVA>
2019-01-27 13:22:47 +0100

    adapt run to new iorx paradigm

--- iorx/src/main/java/ubiquisense/iorx/comm/udp/io/UdpInputPortJob.java
+++ iorx/src/main/java/ubiquisense/iorx/comm/udp/io/UdpInputPortJob.java
@@ -5,7 +5,6 @@ import ubiquisense.iorx.comm.AbstractInputJob;
 import ubiquisense.iorx.io.IXFrameInterpreter;
 import ubiquisense.iorx.qx.Rx;
 
-
 public class UdpInputPortJob extends AbstractInputJob {
 	private volatile UdpRcvChannel channel;
 	private int portID;
@@ -15,22 +14,27 @@ public class UdpInputPortJob extends AbstractInputJob {
 		this.portID = port;
 		channel = new UdpRcvChannel(port);
 	}
+
 	public int getPortID() {
 		return portID;
 	}
+
+	@Override
+	public synchronized void start() {
+		super.start();
+	}
+
 	@Override
 	public void run() {
-		synchronized(channel) {
-			if (channel.connected()) {
+		while (channel.connected()) {
+			synchronized (channel) {
 				channel.receive(new byte[15360]);
 				for (Rx rx : rxList) {
 					synchronized (rx.getEngine().getInputInterpreter()) {
 						IXFrameInterpreter frameInterpreter = rx.getEngine().getInputInterpreter();
 						if (frameInterpreter != null) {
 							try {
-								rx.postCommand(
-									frameInterpreter.byteArray2Cmd(channel.getData())
-								);
+								rx.postCommand(frameInterpreter.byteArray2Cmd(channel.getData()));
 							} catch (Throwable t) {
 								t.printStackTrace();
 							}
@@ -41,8 +45,8 @@ public class UdpInputPortJob extends AbstractInputJob {
 				}
 			}
 		}
-		start();
 	}
+
 	@Override
 	public synchronized void close() {
 		channel.close();
